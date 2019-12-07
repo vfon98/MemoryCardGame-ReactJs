f133da9e4bba692e72c085b15e19f9aa74d48e61
vfon98 <vfon98@gmail.com>
2019-12-07 16:02:34 +0700

    Updates

---
+++ precache-manifest.d4b15a17cd13039482fd907e14423b18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226e1c291c1719be4080e8947a215ec7",
+    "url": "/MemoryCardGame-ReactJs/index.html"
+  },
+  {
+    "revision": "b00d092d68548f721251",
+    "url": "/MemoryCardGame-ReactJs/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "8982069129c1a17091f3",
+    "url": "/MemoryCardGame-ReactJs/static/css/main.951b7187.chunk.css"
+  },
+  {
+    "revision": "b00d092d68548f721251",
+    "url": "/MemoryCardGame-ReactJs/static/js/2.be2c1681.chunk.js"
+  },
+  {
+    "revision": "8982069129c1a17091f3",
+    "url": "/MemoryCardGame-ReactJs/static/js/main.7362e979.chunk.js"
+  },
+  {
+    "revision": "255fd5f8ea776987278b",
+    "url": "/MemoryCardGame-ReactJs/static/js/runtime-main.f49139e9.js"
+  }
+]);
